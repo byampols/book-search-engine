30e3d4308ff0236104bfd3bb7ba89b92b20efb4c
unknown <byampols@alumni.cmu.edu>
2021-12-26 17:39:06 -0500

    updated server to graphql, added schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: Book): User
+        remove(bookId: String!): User
+    }
+`
+
+module.exports = typeDefs;
