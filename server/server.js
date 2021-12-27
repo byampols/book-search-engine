7c3b31cf42083ce046a304fa3928565a74fb40b6
unknown <byampols@alumni.cmu.edu>
2021-12-26 23:52:34 -0500

    added option for secret env

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const {typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection');
-// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -31,8 +30,6 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
